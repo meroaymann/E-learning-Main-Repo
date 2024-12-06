f0d59378fd4632eeffafc733c622efe3b5c8ef21
Ahmedehab5193 <ahmedehab5193@gmail.com>
2024-12-07 00:14:58 +0200

    modules service.ts and controllers.ts

--- src/modules/modules.controller.ts
+++ src/modules/modules.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+// controllers.ts
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {  ModuleService } from './modules.service';
+// import { ModulesSchema } from './modules.schema';
+import { CreateModulesDto } from './DTOs/CreateModuleDto';
+import { UpdateModulesDto } from './DTOs/UpdateModuleDto';
+import { Module } from './models/modules.schema';
 
 @Controller('modules')
-export class ModulesController {}
+export class ModuleController {
+  constructor(private readonly ModuleService: ModuleService) {}
+
+  @Post()
+  async create(@Body() createModuleDto: CreateModulesDto): Promise<Module> {
+    return this. ModuleService.create(createModuleDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Module[]> {
+    return this. ModuleService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Module> {
+    return this. ModuleService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateModuleDto: UpdateModulesDto): Promise<Module> {
+    return this. ModuleService.update(id, updateModuleDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<{ message: string }> {
+    await this. ModuleService.delete(id);
+    return { message: 'Module successfully deleted' };
+  }
+}
--- src/modules/modules.service.ts
+++ src/modules/modules.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Module, ModuleDocument } from './models/modules.schema';
+import { CreateModulesDto } from './DTOs/CreateModuleDto';
+import { UpdateModulesDto } from './DTOs/UpdateModuleDto';
 
 @Injectable()
-export class ModulesService {}
+export class ModuleService {
+  constructor(@InjectModel(Module.name) private moduleModel: Model<ModuleDocument>) {}
+
+  async create(createModuleDto: CreateModulesDto): Promise<Module> {
+    const createdModule = new this.moduleModel(createModuleDto);
+    return createdModule.save();
+  }
+
+  async findAll(): Promise<Module[]> {
+    return this.moduleModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Module> {
+    const module = await this.moduleModel.findById(id).exec();
+    if (!module) {
+      throw new NotFoundException(`Module with ID ${id} not found`);
+    }
+    return module;
+  }
+
+  async update(id: string, updateModuleDto: UpdateModulesDto): Promise<Module> {
+    const updatedModule = await this.moduleModel.findByIdAndUpdate(id, updateModuleDto, { new: true }).exec();
+    if (!updatedModule) {
+      throw new NotFoundException(`Module with ID ${id} not found`);
+    }
+    return updatedModule;
+}
+
+async delete(id: string): Promise<void> {
+  const result = await this.moduleModel.findByIdAndDelete(id).exec();
+  if (!result) {
+    throw new NotFoundException(`Module with ID ${id} not found`);
+  }
+}
+}
